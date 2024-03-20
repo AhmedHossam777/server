03ff6a303da794d192b08b1d76449146d05b43e2
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-20 07:48:06 +0200

    finish simple stripe integration

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
 require('dotenv').config();
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://127.0.0.1:5500',
+  })
+);
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const storeItems = new Map([
+  [1, { priceInCents: 10000, name: 'Learn React Today' }],
+  [2, { priceInCents: 20000, name: 'Learn CSS Today' }],
+]);
+
+app.post('/create-checkout-session', async (req, res, next) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment', // if you wanna do subscription we will put subscription,
+      line_items: req.body.items.map((item) => {
+        const storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        };
+      }), // line items is what the user is paying for
+      success_url: `${process.env.CLIENT_URL}/success.html`, // where we will send the user after success payment
+      cancel_url: `${process.env.CLIENT_URL}/cancel.html`, // where we will send the user after cancel payment
+    });
+    res.json({ url: session.url });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
